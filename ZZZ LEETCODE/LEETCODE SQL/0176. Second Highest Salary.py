4eb566e4859b88ecad77cfc5453cb692812a9d02
MLV Prasad <iamprasad23@gmail.com>
2022-08-11 12:25:50 +0530

    ok

--- ZZZ LEETCODE/LEETCODE SQL/0176. Second Highest Salary.py
+++ ZZZ LEETCODE/LEETCODE SQL/0176. Second Highest Salary.py
@@ -19,17 +19,17 @@ Table: Employee
 +-------------+------+
 id is the primary key column for this table.
 Each row of this table contains information about the salary of an employee.
- 
+
 
 Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
 
 The query result format is in the following example.
 
- 
 
+ok
 Example 1:
 
-Input: 
+Input:
 Employee table:
 +----+--------+
 | id | salary |
@@ -38,7 +38,7 @@ Employee table:
 | 2  | 200    |
 | 3  | 300    |
 +----+--------+
-Output: 
+Output:
 +---------------------+
 | SecondHighestSalary |
 +---------------------+
@@ -46,14 +46,14 @@ Output:
 +---------------------+
 Example 2:
 
-Input: 
+Input:
 Employee table:
 +----+--------+
 | id | salary |
 +----+--------+
 | 1  | 100    |
 +----+--------+
-Output: 
+Output:
 +---------------------+
 | SecondHighestSalary |
 +---------------------+
